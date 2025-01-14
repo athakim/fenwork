69a61e856c13f166cebbe4fe0b8ea71054bbd45c
hakim aittayeb <athakim95@gmail.com>
2025-01-14 10:45:21 +0100

    initial commit , init spring boot project

---
+++ src/main/java/com/kodoops/fenwork/FenworkApplication.java
@@ -0,0 +1,13 @@
+package com.kodoops.fenwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FenworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FenworkApplication.class, args);
+    }
+
+}
